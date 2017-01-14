a1c963d8d7d1308e9bdd55a987b284ccc98a9369
Zain Ali <zain_ali8@hotmail.com>
2017-01-14 18:03:00 +1300

    Function use example

---
+++ Examples/7segExample/_7segExample/_7segExample.ino
@@ -0,0 +1,45 @@
+#include <SevenSegment.h>
+
+// Seven segment display pins
+const int sPinA = 3;
+const int sPinB = 4;
+const int sPinC = 5;
+const int sPinD = 6;
+const int sPinE = 7;
+const int sPinF = 8;
+const int sPinG = 9;
+
+// Display
+SevenSegment disp = SevenSegment(sPinA,sPinB,sPinC,sPinD,sPinE,sPinF,sPinG,COMMON_CATHODE);
+//SevenSegment disp = SevenSegment(sPinA,sPinB,sPinC,sPinD,sPinE,sPinF,sPinG,COMMON_ANODE);
+
+void setup() {
+  // Blank out display
+  disp.clear();
+}
+
+void loop() {
+  int i;
+  // Loop through segments animation x 2, 100 ms segment delay
+  disp.segLoop(2,100);
+  disp.clear();// Clear isplay
+
+  // Write numbers with no delay
+  for( i = 9; i >= 0 ; i--)
+  {
+    disp.numWrite(i);
+    delay(500);
+  }
+
+  // Loop through segments animation 50 ms delay
+  disp.segLoop(1,50);
+  disp.clear(); // Clear isplay
+
+  // Write numbers with segment delay 50ms
+  for( i = 9; i >= 0 ; i--)
+  {
+    disp.numWrite(i,50);
+    delay(500);
+  }
+
+}
