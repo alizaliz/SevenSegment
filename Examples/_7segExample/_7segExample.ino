aada42e1a0b6abbecd2247243b9cfa36a611b2fb
Zain Ali <zain_ali8@hotmail.com>
2019-03-25 23:12:40 +1300

    Update to literals for Arduino IDE. Example sketch rebuilt to work with new library.

--- Examples/_7segExample/_7segExample.ino
+++ Examples/_7segExample/_7segExample.ino
@@ -11,37 +11,38 @@ const int sPinG = 9;
 
 // Display
 SevenSegment disp = SevenSegment();
+int i = -1;
+int num = 0;
 
 void setup() {
   disp.attach(sPinA, sPinB, sPinC, sPinD, sPinE, sPinF, sPinG);
 
-  //disp.type(COMMON_ANODE)
+  disp.type(COMMON_ANODE);
   // Blank out display
   disp.clear();
+  disp.interval(100);
 }
 
 void loop() {
-  int i;
-  // Loop through segments animation x 2, 100 ms segment delay
-  disp.segLoop(2,100);
-  disp.clear();// Clear isplay
+  disp.update();
 
-  // Write numbers with no delay
-  for( i = 9; i >= 0 ; i--)
+  if(disp.currentState() == REST)
+  {
+    i++;
+  }
+  
+  if(i == 0)
+  {
+    // Loop through segments animation x 2, 100 ms segment delay
+    disp.segLoop(2);
+  }
+  else if ( i > 0 && i <= 9)
   {
     disp.numWrite(i);
-    delay(500);
   }
-
-  // Loop through segments animation 50 ms delay
-  disp.segLoop(1,50);
-  disp.clear(); // Clear isplay
-
-  // Write numbers with segment delay 50ms
-  for( i = 9; i >= 0 ; i--)
+  else
   {
-    disp.numWrite(i,50);
-    delay(500);
+    i = 0;
   }
 
 }
